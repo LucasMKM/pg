9d04aa915e614c693124c873f8b97b5aa97ee485
LucasMKM <2024314576@aluno.riogrande.ifrs.edu.br>
2025-11-14 09:10:19 -0300

    Add validation for task input fields
    Added validation to ensure all fields are filled before adding a task.

--- js/app.js
+++ js/app.js
@@ -22,6 +22,10 @@ function mostra() {
 }
 
 function insereTarefa() {
+    if (!tarefa || !responsavel || !inicio || !fim) {
+    alert("Por favor, preencha todos os campos antes de adicionar a tarefa!");
+    return;
+      
   const novaTarefa = {
     tarefa: document.querySelector("#tarefa").value,
     responsavel: document.querySelector("#responsavel").value,
@@ -65,6 +69,7 @@ function limpar() {
   mostra();
   alert("Lista limpa com sucesso!");
 }
+
 
 
 
